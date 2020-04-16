f84b2db097689183dabb753cc5ebbe9e68aed8f5
Matheus Pegoraro <matheuspegoraro@icloud.com>
2020-04-16 15:18:19 -0300

    Adicionando Whoollie Food App

--- js/data.js
+++ js/data.js
@@ -1,52 +1,42 @@
 const programmingLanguages = [
   {
-    icon: `
-      <span class="fa-stack fa-1x">
-        <i class="fas fa-circle fa-stack-2x"></i>
-        <i class="fab fa-js fa-stack-1x fa-inverse"></i>
-      </span>`,
+    iconType: 'icon',
+    icon: `fa-js`,
     type: 'Linguagem de Programação',
     name: 'JavaScript',
     experience: 'Avançado - 2 anos'
   },
   {
-    icon: `
-      <span class="fa-stack fa-1x">
-        <i class="fas fa-circle fa-stack-2x"></i>
-        <i class="fab fa-php fa-stack-1x fa-inverse"></i>
-      </span>`,
+    iconType: 'icon',
+    icon: `fa-php`,
     name: 'PHP 7',
     type: 'Linguagem de Programação',
     experience: 'Avançado - 2 anos'
   },
   {
-    icon: '<img src="img/skills/dotnet.png" width="40">',
+    iconType: 'img',
+    icon: 'img/skills/dotnet.png',
     name: '.NET FRAMEWORK - VB e C#',
     type: 'Linguagem de Programação',
     experience: 'Intermediário - 2 anos'
   },
   {
-    icon: `
-      <span class="fa-stack fa-1x">
-        <i class="fas fa-circle fa-stack-2x"></i>
-        <i class="fab fa-python fa-stack-1x fa-inverse"></i>
-      </span>`,
+    iconType: 'icon',
+    icon: `fa-python`,
     name: 'Python 3',
     type: 'Linguagem de Programação',
     experience: 'Básico - 3 meses'
   },
   {
-    icon: `
-      <span class="fa-stack fa-1x">
-        <i class="fas fa-circle fa-stack-2x"></i>
-        <i class="fab fa-java fa-stack-1x fa-inverse"></i>
-      </span>`,
+    iconType: 'icon',
+    icon: `fa-java`,
     name: 'Java',
     type: 'Linguagem de Programação',
     experience: 'Básico - 6 meses'
   },
   {
-    icon: '<img src="img/skills/advpl.png" width="40">',
+    iconType: 'img',
+    icon: 'img/skills/advpl.png',
     name: 'ADVPL',
     type: 'Linguagem de Programação',
     experience: 'Intermediário - 6 meses'
@@ -55,47 +45,36 @@ const programmingLanguages = [
 
 const frameworks = [
   {
-    icon: `
-      <span class="fa-stack fa-1x">
-        <i class="fas fa-circle fa-stack-2x"></i>
-        <i class="fab fa-react fa-stack-1x fa-inverse"></i>
-      </span>`,
+    iconType: 'icon',
+    icon: `fa-react`,
     name: 'ReactJS',
     type: 'Framework',
     experience: 'Intermediário - 1 ano'
   },
   {
-    icon: `
-      <span class="fa-stack fa-1x">
-        <i class="fas fa-circle fa-stack-2x"></i>
-        <i class="fab fa-react fa-stack-1x fa-inverse"></i>
-      </span>`,
+    iconType: 'icon',
+    icon: `fa-react`,
     name: 'React Native',
     type: 'Framework',
     experience: 'Intermediário - 6 meses'
   },
   {
-    icon: `
-      <span class="fa-stack fa-1x">
-        <i class="fas fa-circle fa-stack-2x"></i>
-        <i class="fab fa-php fa-stack-1x fa-inverse"></i>
-      </span>`,
+    iconType: 'icon',
+    icon: `fa-php`,
     name: 'PHP MVC - Slim Framework',
     type: 'Framework',
     experience: 'Avançado - 2 anos'
   },
   {
-    icon: '<img src="img/skills/jquery.png" width="40">',
+    iconType: 'img',
+    icon: 'img/skills/jquery.png',
     name: 'jQuery',
     type: 'Framework',
     experience: 'Avançado - 2 anos'
   },
   {
-    icon: `
-      <span class="fa-stack fa-1x">
-        <i class="fas fa-circle fa-stack-2x"></i>
-        <i class="fab fa-bootstrap fa-stack-1x fa-inverse"></i>
-      </span>`,
+    iconType: 'icon',
+    icon: `fa-bootstrap`,
     name: 'Bootstrap Framework',
     type: 'Framework',
     experience: 'Avançado - 2 anos'
@@ -104,21 +83,15 @@ const frameworks = [
 
 const notProgramingLanguages = [
   {
-    icon: `
-      <span class="fa-stack fa-1x">
-        <i class="fas fa-circle fa-stack-2x"></i>
-        <i class="fab fa-html5 fa-stack-1x fa-inverse"></i>
-      </span>`,
+    iconType: 'icon',
+    icon: `fa-html5`,
     name: 'HTML 5',
     type: 'Linguagem não programável',
     experience: 'Avançado - 3 anos'
   },
   {
-    icon: `
-      <span class="fa-stack fa-1x">
-        <i class="fas fa-circle fa-stack-2x"></i>
-        <i class="fab fa-css3-alt fa-stack-1x fa-inverse"></i>
-      </span>`,
+    iconType: 'icon',
+    icon: `fa-css3-alt`,
     name: 'CSS 3',
     type: 'Linguagem não programável',
     experience: 'Intermediário - 3 anos'
@@ -127,13 +100,15 @@ const notProgramingLanguages = [
 
 const architectures = [
   {
-    icon: '<img src="img/skills/rest.png" width="40">',
+    iconType: 'img',
+    icon: 'img/skills/rest.png',
     name: 'REST',
     type: 'Arquitetura',
     experience: 'Intermediário - 2 anos'
   },
   {
-    icon: '<img src="img/skills/rest.png" width="40">',
+    iconType: 'img',
+    icon: 'img/skills/rest.png',
     name: 'RESTFUL',
     type: 'Arquitetura',
     experience: 'Intermediário - 2 anos'
@@ -142,21 +117,15 @@ const architectures = [
 
 const tools = [
   {
-    icon: `
-      <span class="fa-stack fa-1x">
-        <i class="fas fa-circle fa-stack-2x"></i>
-        <i class="fab fa-node-js fa-stack-1x fa-inverse"></i>
-      </span>`,
+    iconType: 'icon',
+    icon: `fa-node-js`,
     name: 'NodeJS',
     type: 'Ferramenta',
     experience: 'Intermediário - 1 anos'
   },
   {
-    icon: `
-      <span class="fa-stack fa-1x">
-        <i class="fas fa-circle fa-stack-2x"></i>
-        <i class="fab fa-npm fa-stack-1x fa-inverse"></i>
-      </span>`,
+    iconType: 'icon',
+    icon: `fa-npm`,
     name: 'NPM',
     type: 'Ferramenta',
     experience: 'Intermediário - 1 anos'
@@ -165,13 +134,15 @@ const tools = [
 
 const databases = [
   {
-    icon: '<img src="img/skills/mysql.png" width="40">',
+    iconType: 'img',
+    icon: 'img/skills/mysql.png',
     name: 'MySQL',
     type: 'Banco de Dados',
     experience: 'Intermediário - 3 anos'
   },
   {
-    icon: '<img src="img/skills/sql-server.png" width="40">',
+    iconType: 'img',
+    icon: 'img/skills/sql-server.png',
     name: 'SQLServer',
     type: 'Banco de Dados',
     experience: 'Intermediário - 2 anos'
@@ -180,13 +151,15 @@ const databases = [
 
 const softwares = [
   {
-    icon: '<img src="img/skills/corel.png" width="40">',
+    iconType: 'img',
+    icon: 'img/skills/corel.png',
     name: 'Corel Draw',
     type: 'Software',
     experience: 'Avançado - 5 anos'
   },
   {
-    icon: '<img src="img/skills/protheus.png" width="40">',
+    iconType: 'img',
+    icon: 'img/skills/protheus.png',
     name: 'Protheus',
     type: 'Software',
     experience: 'Intermediário - 1 ano e 6 meses'
@@ -241,14 +214,35 @@ const lectures = [
 
 const projects = [
   {
-    name: "Whoollie Food",
+    id: 'whoolliefoodweb',
+    name: "Whoollie Food - WEB",
+    description: 'Um Software para gerenciamento e administração de bares, restaurantes e lanchonetes de usabilidade ágil, proporcionando ao responsável do estabelecimento o controle sobre: o acesso de funcionários, edição do cardápio e monitoramento dos pedidos. Já o cliente tem uma plataforma mobile de autoatendimento intuitiva e interativa que também diminui o tempo de espera para o cliente ser atendimento',
+    isApp: false,
     mockups: [
       { name: "Tela de Login", path: "img/projects/whoollie01.png", active: "active" },
       { name: "Dashboard com Gráficos", path: "img/projects/whoollie02.png", active: "" },
       { name: "Controle de Produtos", path: "img/projects/whoollie03.png", active: "" },
       { name: "Controle de Categorias de Produtos", path: "img/projects/whoollie04.png", active: "" },
       { name: "Controle de Ingredientes", path: "img/projects/whoollie05.png", active: "" },
-      { name: "Monitor de Pedidos para Cozinha", path: "img/projects/whoollie06.png", active: "" }
+      { name: "Monitor de Pedidos para Cozinha", path: "img/projects/whoollie06.png", active: "" },
+      { name: "Monitor de Pedidos para Cozinha Com Pedido Sendo Liberado", path: "img/projects/whoollie07.png", active: "" },
+      { name: "Controle de Comandas", path: "img/projects/whoollie08.png", active: "" },
+      { name: "Informação de Uma Comanda Aberta", path: "img/projects/whoollie09.png", active: "" },
+    ]
+  },
+  {
+    id: 'whoolliefoodmobile',
+    name: "Whoollie Food - MOBILE",
+    description: 'Um aplicativo onde permite o cliente pode montar o carrinho conforme seu interesse, efetuar um ou mais pedidos, visualizar sua própria comanda e chamar o garçom, e tudo isto em tempo real!',
+    isApp: true,
+    mockups: [
+      { name: "Menu do Aplicativo", path: "img/projects/whoollie-app01.png", active: "active" },
+      { name: "Lista de Categorias do Menu", path: "img/projects/whoollie-app02.png", active: "" },
+      { name: "Informação de um Produto", path: "img/projects/whoollie-app03.png", active: "" },
+      { name: "Adicionando um Produto ao Carrinho", path: "img/projects/whoollie-app04.png", active: "" },
+      { name: "Listagem do Carrinho", path: "img/projects/whoollie-app05.png", active: "" },
+      { name: "Enviando o Carrinho para a Cozinha", path: "img/projects/whoollie-app07.png", active: "" },
+      { name: "Listagem da Comanda Aberta com os Pedidos Realizados", path: "img/projects/whoollie-app06.png", active: "" },
     ]
   }
 ]
@@ -259,6 +253,19 @@ $(() => {
     if (b.name > a.name) return -1;
   
     return 0;
+  }
+
+  function getIcon(iconType, icon) {
+    if (iconType == 'img') {
+      return `<img src="${icon}" width="40">`;
+    } else {
+      return `
+        <span class="fa-stack fa-1x">
+          <i class="fas fa-circle fa-stack-2x"></i>
+          <i class="fab ${icon} fa-stack-1x fa-inverse"></i>
+        </span>
+      `;
+    }
   }
   
   let skills = [];
@@ -273,7 +280,7 @@ $(() => {
   skills.map((skill) => {
     $('.skills-list').append(`
       <tr>
-        <td class="text-primary text-center">${skill.icon}</td>
+        <td class="text-primary text-center">${getIcon(skill.iconType, skill.icon)}</td>
         <td>${skill.type}</td>
         <td class="text-bold">${skill.name}</td>
         <td>${skill.experience}</td>
@@ -312,29 +319,33 @@ $(() => {
 
     let imagesHTML = '';
 
+    let isApp = project.isApp;
+
     project.mockups.map(mockup => {
       imagesHTML += `
         <div class="carousel-item ${mockup.active}">
-          <img class="d-block w-100" src="${mockup.path}" alt="${mockup.name}">
+          <img class="d-block ${isApp ? 'img-slider' : 'w-100'}" src="${mockup.path}" alt="${mockup.name}">
         </div>
       `;
     });
 
     $('.projects-list').append(`
-      <h3 class="w-100 text-center">${project.name}</h3>
-      <h6 class="w-100 text-center text-non-bold">Um Software para gerenciamento e administração de bares, restaurantes e lanchonetes de usabilidade ágil, proporcionando ao responsável do estabelecimento o controle sobre: o acesso de funcionários, edição do cardápio e monitoramento dos pedidos. Já o cliente tem uma plataforma mobile de autoatendimento intuitiva e interativa que também diminui o tempo de espera para o cliente ser atendimento</h6>
-      <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-        <div class="carousel-inner">
-          ${imagesHTML}
+      <div class="carousel-container">
+        <h3>${project.name}</h3>
+        <p class="text-non-bold">${project.description}</p>
+        <div id="${project.id}" class="carousel slide ${isApp ? 'carousel-app' : ''} carousel-centered" data-ride="carousel">
+          <div class="carousel-inner ${isApp ? 'carousel-inner-app' : ''}">
+            ${imagesHTML}
+          </div>
+          <a class="carousel-control-prev" href="#${project.id}" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Voltar</span>
+          </a>
+          <a class="carousel-control-next" href="#${project.id}" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Próximo</span>
+          </a>
         </div>
-        <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="sr-only">Voltar</span>
-        </a>
-        <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="sr-only">Próximo</span>
-        </a>
       </div>
     `);
   })
