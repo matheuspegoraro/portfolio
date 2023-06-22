4abf4f5e34e45c5dde7ff33d0e18924977e940d3
Matheus Pegoraro <matheuspegoraro@icloud.com>
2023-06-22 11:15:03 -0300

    updates

--- js/data.js
+++ js/data.js
@@ -2,51 +2,51 @@ const programmingLanguages = [
   {
     iconType: 'icon',
     icon: 'fa-js',
-    type: 'Linguagem de Programação',
+    type: 'Programming language',
     name: 'JavaScript',
-    experience: 'Avançado - 2 anos'
+    experience: 'Advanced - 4 years'
   },
   {
     iconType: 'icon',
     icon: 'fa-php',
-    name: 'PHP 7',
-    type: 'Linguagem de Programação',
-    experience: 'Avançado - 2 anos'
+    name: 'PHP 7 and 8',
+    type: 'Programming language',
+    experience: 'Advanced - 3 years'
   },
   {
     iconType: 'img',
     icon: 'img/skills/dotnet.png',
-    name: '.NET Core 3.1, 5 e 6',
-    type: 'Linguagem de Programação',
-      experience: 'Avançado - 4 anos'
+    name: '.NET Core 3.1, 5, 6 and 8',
+    type: 'Programming language',
+      experience: 'Advanced - 4 years'
   },
   {
     iconType: 'img',
     icon: 'img/skills/dotnet.png',
     name: '.NET FRAMEWORK - VB e C#',
-    type: 'Linguagem de Programação',
-      experience: 'Avançado - 6 anos'
+    type: 'Programming language',
+      experience: 'Advanced - 6 years'
   },
   {
     iconType: 'icon',
     icon: 'fa-python',
     name: 'Python 3',
-    type: 'Linguagem de Programação',
-    experience: 'Básico - 3 meses'
+    type: 'Programming language',
+    experience: 'Básico - 3 months'
   },
   {
     iconType: 'icon',
     icon: 'fa-java',
     name: 'Java',
-    type: 'Linguagem de Programação',
-    experience: 'Básico - 6 meses'
+    type: 'Programming language',
+    experience: 'Básico - 6 months'
   },
   {
     iconType: 'img',
     icon: 'img/skills/advpl.png',
     name: 'ADVPL',
-    type: 'Linguagem de Programação',
-    experience: 'Intermediário - 6 meses'
+    type: 'Programming language',
+    experience: 'Intermediary - 6 months'
   },
 ];
 
@@ -56,35 +56,42 @@ const frameworks = [
     icon: 'fa-react',
     name: 'ReactJS',
     type: 'Framework',
-    experience: 'Avançado - 3 anos'
+    experience: 'Advanced - 4 years'
   },
   {
     iconType: 'icon',
     icon: 'fa-react',
     name: 'NextJS',
     type: 'Framework',
-    experience: 'Avançado - 1 ano'
+    experience: 'Advanced - 2 year'
   },
   {
     iconType: 'icon',
     icon: 'fa-react',
     name: 'React Native',
     type: 'Framework',
-    experience: 'Avançado - 1 ano e 6 meses'
+    experience: 'Advanced - 3 months'
   },
   {
     iconType: 'icon',
     icon: 'fa-php',
     name: 'PHP MVC - Slim Framework',
     type: 'Framework',
-    experience: 'Avançado - 2 anos'
+    experience: 'Advanced - 2 years'
+  },
+  {
+    iconType: 'icon',
+    icon: 'fa-php',
+    name: 'PHP Laravel',
+    type: 'Framework',
+    experience: 'Advanced - 2 years'
   },
   {
     iconType: 'img',
     icon: 'img/skills/jquery.png',
     name: 'jQuery',
     type: 'Framework',
-    experience: 'Avançado - 2 anos'
+    experience: 'Advanced - 5 years'
   },
 ];
 
@@ -94,14 +101,14 @@ const notProgramingLanguages = [
     icon: 'fa-html5',
     name: 'HTML 5',
     type: 'Linguagem não programável',
-    experience: 'Avançado - 5 anos'
+    experience: 'Advanced - 6 years'
   },
   {
     iconType: 'icon',
     icon: 'fa-css3-alt',
     name: 'CSS 3',
     type: 'Linguagem não programável',
-    experience: 'Intermediário - 3 anos'
+    experience: 'Intermediary - 5 years'
   },
 ];
 
@@ -111,14 +118,14 @@ const architectures = [
     icon: 'img/skills/rest.png',
     name: 'REST',
     type: 'Arquitetura',
-    experience: 'Intermediário - 3 anos'
+    experience: 'Intermediary - 5 years'
   },
   {
     iconType: 'img',
     icon: 'img/skills/rest.png',
     name: 'RESTFUL',
     type: 'Arquitetura',
-    experience: 'Intermediário - 2 anos'
+    experience: 'Intermediary - 5 years'
   },
 ];
 
@@ -128,7 +135,7 @@ const tools = [
     icon: 'fa-node-js',
     name: 'NodeJS',
     type: 'Ferramenta',
-    experience: 'Intermediário - 1 anos'
+    experience: 'Intermediary - 2 year'
   },
 ];
 
@@ -137,15 +144,22 @@ const databases = [
     iconType: 'img',
     icon: 'img/skills/mysql.png',
     name: 'MySQL',
-    type: 'Banco de Dados',
-        experience: 'Avançado - 3 anos'
+    type: 'Database',
+        experience: 'Advanced - 5 years'
   },
   {
     iconType: 'img',
     icon: 'img/skills/sql-server.png',
     name: 'SQLServer',
-    type: 'Banco de Dados',
-      experience: 'Avançado - 4 anos'
+    type: 'Database',
+      experience: 'Advanced - 5 years'
+  },
+  {
+    iconType: 'img',
+    icon: 'img/skills/sql-server.png',
+    name: 'Oracle',
+    type: 'Database',
+      experience: 'Advanced - 3 years'
   },
 ];
 
@@ -155,61 +169,61 @@ const softwares = [
     icon: 'img/skills/protheus.png',
     name: 'Protheus',
     type: 'Software',
-    experience: 'Intermediário - 1 ano e 6 meses'
+    experience: 'Intermediary - 1 year and 6 months'
   },
 ];
 
 const courses = [
-  { name: 'CRIE UMA CALCULADORA DE PINTURA COM C# - DEVMEDIA' },
-  { name: 'AUTENTICAÇÃO NO ASP.NET MVC - DEVMEDIA' },
-  { name: 'ASP.NET MVC E IDENTITY: AUTORIZAÇÃO DE USUÁRIOS COM CLAIMS - DEVMEDIA' },
-  { name: 'MINHA PRIMEIRA APLICAÇÃO C# - DEVMEDIA' },
-  { name: 'ASP.NET MVC E ENTITY FRAMEWORK: CRIANDO UMA APLICAÇÃO 1:N - DEVMEDIA' },
+  { name: 'CREATE A PAINTING CALCULATOR WITH C# - DEVMEDIA' },
+  { name: 'AUTHENTICATION IN ASP.NET MVC - DEVMEDIA' },
+  { name: 'ASP.NET MVC AND IDENTITY: AUTHORIZATION OF USERS WITH CLAIMS - DEVMEDIA' },
+  { name: 'MY FIRST C# APPLICATION - DEVMEDIA' },
+  { name: 'ASP.NET MVC AND ENTITY FRAMEWORK: CREATING A 1:N APPLICATION - DEVMEDIA' },
   { name: 'ENTITY FRAMEWORK MIGRATIONS - DEVMEDIA' },
-  { name: 'MAPEAMENTO 1:N NO ENTITY FRAMEWORK - DEVMEDIA' },
-  { name: 'PREPARANDO O AMBIENTE PARA PROGRAMAR EM .NET - DEVMEDIA' },
-  { name: 'O QUE É NUGET? - DEVMEDIA' },
-  { name: 'ASP.NET MVC: PRIMEIRA APLICAÇÃO COM O ENTITY FRAMEWORK - DEVMEDIA' },
-  { name: 'O QUE É .NET? - DEVMEDIA' },
-  { name: 'O QUE É C#? - DEVMEDIA' },
-  { name: 'O QUE É ASP.NET WEB API? - DEVMEDIA' },
-  { name: 'O QUE É ASP.NET MVC? - DEVMEDIA' },
+  { name: '1:N MAPPING NO ENTITY FRAMEWORK - DEVMEDIA' },
+  { name: 'PREPARING THE ENVIRONMENT TO PROGRAM IN .NET - DEVMEDIA' },
+  { name: 'WHAT IS NUGET? - DEVMEDIA' },
+  { name: 'ASP.NET MVC: FIRST APPLICATION WITH ENTITY FRAMEWORK - DEVMEDIA' },
+  { name: 'WHAT IS .NET? - DEVMEDIA' },
+  { name: 'WHAT IS C#? - DEVMEDIA' },
+  { name: 'WHAT IS ASP.NET WEB API? - DEVMEDIA' },
+  { name: 'WHAT IS ASP.NET MVC? - DEVMEDIA' },
   { name: 'ASP.NET MVC - DEVMEDIA' },
-  { name: 'ENTITY FRAMEWORK: PRIMEIROS PASSOS - DEVMEDIA' },
-  { name: 'CONSUMINDO UM SERVIÇO REST COM ASP.NET MVC - DEVMEDIA' },
-  { name: 'CRIANDO SERVIÇOS RESTFUL EM .NET - DEVMEDIA' },
-  { name: 'INTRODUÇÃO AO .NET FRAMEWORK - DEVMEDIA' },
-  { name: 'INTRODUÇÃO A PROGRAMAÇÃO COM C# - DEVMEDIA' },
-  { name: 'CONHECENDO OS RECURSOS DO VISUAL STUDIO - DEVMEDIA' },
-  { name: 'O QUE É MVC? - DEVMEDIA' },
-  { name: 'O QUE É RESTFUL? - DEVMEDIA' },
-  { name: 'JAVA NETBEANS BÁSICO - UDEMY' },
+  { name: 'ENTITY FRAMEWORK: FIRST STEPS - DEVMEDIA' },
+  { name: 'CONSUMING A REST SERVICE WITH ASP.NET MVC - DEVMEDIA' },
+  { name: 'CREATING RESTFUL SERVICES IN .NET - DEVMEDIA' },
+  { name: 'INTRODUCTION TO .NET FRAMEWORK - DEVMEDIA' },
+  { name: 'INTRODUCTION TO PROGRAMMING WITH C# - DEVMEDIA' },
+  { name: 'GETTING TO KNOW THE RESOURCES OF VISUAL STUDIO - DEVMEDIA' },
+  { name: 'WHAT IS MVC? - DEVMEDIA' },
+  { name: 'WHAT IS RESTFUL? - DEVMEDIA' },
+  { name: 'JAVA NETBEANS BASIC - UDEMY' },
   { name: 'JQUERY - UDEMY' },
-  { name: 'PHP 7 Completo - UDEMY' },
-  { name: 'ALGORITMOS - CURSOEMVIDEO' },
+  { name: 'PHP 7 Complete - UDEMY' },
+  { name: 'ALGORITHMS - VIDEO COURSE' },
   { name: 'HTML5 - CURSOEMVIDEO' },
   { name: 'JAVA - CURSOEMVIDEO' },
   { name: 'MYSQL - CURSOEMVIDEO' },
-  { name: 'PHP BÁSICO - CURSOEMVIDEO' },
-  { name: 'PHP POO - CURSOEMVIDEO' }
+  { name: 'BASIC PHP - VIDEO COURSE' },
+  { name: 'PHP POO - COURSEEMVIDEO' }
 ];
 
 const certifications = [
-  { name: 'Inteligência Artificial Fundamentos - Data Science Academy' }
+  { name: 'Artificial Intelligence Fundamentals - Data Science Academy' }
 ];
 
 const lectures = [
-  { name: '12º CONGRESSO CIENTÍFICO - UNIARARAS' },
-  { name: 'FRAMEWORK PARA DESENVOLVIMENTO WEB: DJANGO' },
-  { name: 'MARKETING PESSOAL E MUDANÇAS EM TEMPO DE CRISE' },
-  { name: 'ANÁLISE FORENSE COMPUTACIONAL: DETECTANDO ROOTKITS EM AMBIENTE WINDOWS' }
+  { name: '12th SCIENTIFIC CONGRESS - UNIARARAS' },
+  { name: 'FRAMEWORK FOR WEB DEVELOPMENT: DJANGO' },
+  { name: 'PERSONAL MARKETING AND CHANGES IN TIME OF CRISIS' },
+  { name: 'COMPUTER FORENSIC ANALYSIS: DETECTING ROOTKITS IN WINDOWS ENVIRONMENT' }
 ];
 
 const projects = [
   {
     id: 'whoolliefoodweb',
     name: 'Whoollie Food - WEB',
-    description: 'Um Software para gerenciamento e administração de bares, restaurantes e lanchonetes de usabilidade ágil, proporcionando ao responsável do estabelecimento o controle sobre: o acesso de funcionários, edição do cardápio e monitoramento dos pedidos. Já o cliente tem uma plataforma mobile de autoatendimento intuitiva e interativa que também diminui o tempo de espera para o cliente ser atendimento',
+    description: 'Software for managing and administering bars, restaurants and snack bars with agile usability, providing the person in charge of the establishment with control over: employee access, editing the menu and monitoring orders. The customer has an intuitive and interactive mobile self-service platform that also reduces the waiting time for the customer to be served',
     github: {
       path: 'https://github.com/eduardogeraldini/whoollie-food',
       icon: 'fa-github'
@@ -220,21 +234,21 @@ const projects = [
     },
     isApp: false,
     mockups: [
-      { name: 'Tela de Login', path: 'img/projects/whoollie01.png', active: 'active' },
-      { name: 'Dashboard com Gráficos', path: 'img/projects/whoollie02.png', active: '' },
-      { name: 'Controle de Produtos', path: 'img/projects/whoollie03.png', active: '' },
-      { name: 'Controle de Categorias de Produtos', path: 'img/projects/whoollie04.png', active: '' },
-      { name: 'Controle de Ingredientes', path: 'img/projects/whoollie05.png', active: '' },
-      { name: 'Monitor de Pedidos para Cozinha', path: 'img/projects/whoollie06.png', active: '' },
-      { name: 'Monitor de Pedidos para Cozinha Com Pedido Sendo Liberado', path: 'img/projects/whoollie07.png', active: '' },
-      { name: 'Controle de Comandas', path: 'img/projects/whoollie08.png', active: '' },
-      { name: 'Informação de Uma Comanda Aberta', path: 'img/projects/whoollie09.png', active: '' },
+      { name: 'Login Screen', path: 'img/projects/whoollie01.png', active: 'active' },
+      { name: 'Dashboard with Charts', path: 'img/projects/whoollie02.png', active: '' },
+      { name: 'Product Control', path: 'img/projects/whoollie03.png', active: '' },
+      { name: 'Product Category Control', path: 'img/projects/whoollie04.png', active: '' },
+      { name: 'Ingredient Control', path: 'img/projects/whoollie05.png', active: '' },
+      { name: 'Kitchen Order Monitor', path: 'img/projects/whoollie06.png', active: '' },
+      { name: 'Kitchen Order Monitor With Order Being Released', path: 'img/projects/whoollie07.png', active: '' },
+      { name: 'Command Control', path: 'img/projects/whoollie08.png', active: '' },
+      { name: 'Information on an Open Command', path: 'img/projects/whoollie09.png', active: '' },
     ]
   },
   {
     id: 'whoolliefoodmobile',
     name: 'Whoollie Food - MOBILE',
-    description: 'Um aplicativo onde permite o cliente pode montar o carrinho conforme seu interesse, efetuar um ou mais pedidos, visualizar sua própria comanda e chamar o garçom, e tudo isto em tempo real!',
+    description: 'An application that allows the customer to assemble the cart according to their interest, place one or more orders, view their own order and call the waiter, and all this in real time!',
     github: {
       path: 'https://github.com/MarlonX19/whoolliefood',
       icon: 'fa-github'
@@ -245,69 +259,69 @@ const projects = [
     },
     isApp: true,
     mockups: [
-      { name: 'Menu do Aplicativo', path: 'img/projects/whoollie-app01.png', active: 'active' },
-      { name: 'Lista de Categorias do Menu', path: 'img/projects/whoollie-app02.png', active: '' },
-      { name: 'Informação de um Produto', path: 'img/projects/whoollie-app03.png', active: '' },
-      { name: 'Adicionando um Produto ao Carrinho', path: 'img/projects/whoollie-app04.png', active: '' },
-      { name: 'Listagem do Carrinho', path: 'img/projects/whoollie-app05.png', active: '' },
-      { name: 'Enviando o Carrinho para a Cozinha', path: 'img/projects/whoollie-app07.png', active: '' },
-      { name: 'Listagem da Comanda Aberta com os Pedidos Realizados', path: 'img/projects/whoollie-app06.png', active: '' },
+      { name: 'Application Menu', path: 'img/projects/whoollie-app01.png', active: 'active' },
+      { name: 'Menu Category List', path: 'img/projects/whoollie-app02.png', active: '' },
+      { name: 'Product Information', path: 'img/projects/whoollie-app03.png', active: '' },
+      { name: 'Adding a Product to Cart', path: 'img/projects/whoollie-app04.png', active: '' },
+      { name: 'Cart Listing', path: 'img/projects/whoollie-app05.png', active: '' },
+      { name: 'Sending Cart to Kitchen', path: 'img/projects/whoollie-app07.png', active: '' },
+      { name: 'List of the Open Command with Requests Made', path: 'img/projects/whoollie-app06.png', active: '' },
     ]
   },
   {
     id: 'omnistack10',
     name: "Omnistack 10.0",
-    description: 'O DevRadar é um projeto que visa conectar desenvolvedores próximos a você que trabalham com as mesmas tecnologias.',
+    description: 'DevRadar is a project that aims to connect developers close to you who work with the same technologies.',
     github: {
       path: 'https://github.com/matheuspegoraro/semana-omnistack-10',
       icon: 'fa-github'
     },
     isApp: false,
     mockups: [
-      { name: "Página Inicial", path: "img/projects/omni1001.png", active: "active" },
+      { name: "Home page", path: "img/projects/omni1001.png", active: "active" },
     ]
   },
   {
     id: 'omnistack10mobile',
     name: "Omnistack 10.0 - MOBILE",
-    description: 'O DevRadar é um projeto que visa conectar desenvolvedores próximos a você que trabalham com as mesmas tecnologias.',
+    description: 'DevRadar is a project that aims to connect developers close to you who work with the same technologies.',
     github: {
       path: 'https://github.com/matheuspegoraro/semana-omnistack-10',
       icon: 'fa-github'
     },
     isApp: true,
     mockups: [
-      { name: "Tela Inicial do Aplicativo", path: "img/projects/omni10-app01.png", active: "active" },
-      { name: "Webview do GitHub", path: "img/projects/omni10-app02.png", active: "" },
+      { name: "Application Home Screen", path: "img/projects/omni10-app01.png", active: "active" },
+      { name: "GitHub Webview", path: "img/projects/omni10-app02.png", active: "" },
     ]
   },
   {
     id: 'nlw01',
     name: "NLW 01",
-    description: 'O App Ecoleta foi a aplicação escolhida pela @Rocketseat para representar a primeira edição da NLW. Se trata de uma aplicação mobile e web que ajuda pessoas a encontrarem pontos de coleta de forma eficiente.',
+    description: 'The Ecoleta App was the application chosen by @Rocketseat to represent the first edition of NLW. It is a mobile and web application that helps people find collection points efficiently.',
     github: {
       path: 'https://github.com/matheuspegoraro/nlw-01',
       icon: 'fa-github'
     },
     isApp: false,
     mockups: [
-      { name: "Página Inicial", path: "img/projects/nlw-01.png", active: "active" },
-      { name: "Página De Cadastro de Ponto de Coleta", path: "img/projects/nlw-02.png" },
+      { name: "Homepage", path: "img/projects/nlw-01.png", active: "active" },
+      { name: "Collection Point Registration Page", path: "img/projects/nlw-02.png" },
     ]
   },
   {
     id: 'nlw01mobile',
     name: "NLW 01 - MOBILE",
-    description: 'O App Ecoleta foi a aplicação escolhida pela @Rocketseat para representar a primeira edição da NLW. Se trata de uma aplicação mobile e web que ajuda pessoas a encontrarem pontos de coleta de forma eficiente.',
+    description: 'The Ecoleta App was the application chosen by @Rocketseat to represent the first edition of NLW. It is a mobile and web application that helps people find collection points efficiently.',
     github: {
       path: 'https://github.com/matheuspegoraro/nlw-01',
       icon: 'fa-github'
     },
     isApp: true,
     mockups: [
-      { name: "Tela Inicial Do Aplicativo", path: "img/projects/nlw-app01.png", active: "active" },
-      { name: "Tela Do Mapa e De Filtro Dos Pontos", path: "img/projects/nlw-app02.png" },
-      { name: "Tela De Informações Dos Pontos", path: "img/projects/nlw-app03.png" },
+      { name: "Application Home Screen", path: "img/projects/nlw-app01.png", active: "active" },
+      { name: "Map and Points Filter Screen", path: "img/projects/nlw-app02.png" },
+      { name: "Point Information Screen", path: "img/projects/nlw-app03.png" },
     ]
   },
 ]
@@ -423,11 +437,11 @@ $(() => {
       carouselContainer += `
         <a class="carousel-control-prev" href="#${project.id}" role="button" data-slide="prev">
           <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="sr-only">Voltar</span>
+          <span class="sr-only">Back</span>
         </a>
         <a class="carousel-control-next" href="#${project.id}" role="button" data-slide="next">
           <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="sr-only">Próximo</span>
+          <span class="sr-only">Next</span>
         </a>`;
     }
 
